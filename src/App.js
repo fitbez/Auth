c654109b796d78605c070616a50255ff4fe00840
fitsum belayneh <fitsu_alt@yahoo.com>
2018-10-19 11:54:07 -0400

    firebase set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import firebase from "firebase";
+import { Header } from "./components/common";
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC_6S9DVrDXajGBxPy9fS2E_fDzOtuFllo",
+      authDomain: "auth-ffb65.firebaseapp.com",
+      databaseURL: "https://auth-ffb65.firebaseio.com",
+      projectId: "auth-ffb65",
+      storageBucket: "auth-ffb65.appspot.com",
+      messagingSenderId: "933993499967"
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App</Text>
+        <Header headerText="Authentication" />
       </View>
     );
   }
