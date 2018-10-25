8ccdd057be767ed5f5f1872e6936bb089e7b5046
fitsum belayneh <fitsu_alt@yahoo.com>
2018-10-24 22:21:05 -0400

    TextInput

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
-import firebase from "firebase";
+import { View } from "react-native";
 import { Header } from "./components/common";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyC_6S9DVrDXajGBxPy9fS2E_fDzOtuFllo",
-      authDomain: "auth-ffb65.firebaseapp.com",
-      databaseURL: "https://auth-ffb65.firebaseio.com",
-      projectId: "auth-ffb65",
-      storageBucket: "auth-ffb65.appspot.com",
-      messagingSenderId: "933993499967"
-    });
-  }
-
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -16,9 +16,7 @@ const styles = {
     shadowOpacity: 0.1,
     shadowRadius: 2,
     elevation: 1,
-    marginleft: 5,
-    marginRight: 5,
-    marginTop: 10
+    margin: 5
   }
 };
 export { Card };
